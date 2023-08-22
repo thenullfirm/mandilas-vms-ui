e245d4744fb8d99e31c2963d139ad63ef86c64e5
nullthefirst <tey@nullnow.com>
2023-08-22 13:06:43 +0100

    Worked on visitor form submit functionality

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 import mainStyles from '@/app/globals.css';
 import FormFieldInput from '@/components/FormField/FormFieldInput';
@@ -8,6 +9,8 @@ import Submit from '@/components/FormField/Submit';
 export default function Home() {
   const [welcome, setWelcome] = useState([]);
   const [employeeData, setEmployees] = useState([]);
+  const [formData, setFormData] = useState({});
+  const [visitorData, setVisitorData] = useState([]);
 
   useEffect(() => {
     fetchWelcome();
@@ -34,17 +37,52 @@ export default function Home() {
     }
   };
 
+  const submitForm = async () => {
+    try {
+      console.log('Form data:', formData);
+      const response = await fetch('https://mandilas-api.onrender.com/visitors', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const responseData = await response.json();
+      setVisitorData(responseData);
+      console.log('Visitor data:', visitorData);
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    const formInfo = event.target;
+
+    let visitorForm = {};
+
+    visitorForm['visitorName'] = formInfo.visitorName.value;
+    visitorForm['visitorEmail'] = formInfo.visitorEmail.value;
+    visitorForm['employee'] = formInfo.employee.value;
+    visitorForm['timeOfVisit'] = formInfo.timeOfVisit.value;
+
+    setFormData(visitorForm);
+    submitForm();
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <h2>{welcome.info}</h2>
-      <form>
-        <FormFieldInput type="text" id="visitor-name" label="Visitor name" />
-        <FormFieldInput type="text" id="visitor-email" label="Visitor email" />
-        <FormFieldSelect employees={employeeData} id="employee-name" label="Employee name" />
-        <FormFieldInput type="text" id="employee-email" label="Employee email" />
-        <FormFieldInput type="datetime-local" id="time-of-visit" label="Time of visitor" />
+      <Link href="/">
+        <h2>{welcome === [] ? 'Loading ...' : welcome.info}</h2>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <FormFieldInput type="text" id="visitorName" label="Visitor name" />
+        <FormFieldInput type="text" id="visitorEmail" label="Visitor email" />
+        <FormFieldSelect employees={employeeData} id="employee" label="Employee name" />
+        <FormFieldInput type="datetime-local" id="timeOfVisit" label="Time of visitor" />
         <Submit title="Schedule Meeting" />
       </form>
+      <hr />
     </div>
   );
 }
