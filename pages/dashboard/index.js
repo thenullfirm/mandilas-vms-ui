f8af20ac8f6723f7cd31e5cd2189a8493a6764e4
nullthefirst <tey@nullnow.com>
2023-09-04 15:44:14 +0100

    Completed login redirect logic

---
+++ pages/dashboard/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import serverUrl from '@/config/serverUrl';
+import { useRouter } from 'next/navigation';
+
+export default function Dashboard() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    getAdmin();
+  }, []);
+
+  const loginRedirect = (info) => {
+    if (!info.loggedIn) {
+      push('/login');
+    }
+  };
+
+  const getAdmin = async () => {
+    try {
+      const response = await fetch(`${serverUrl}/admin`);
+      const responseData = await response.json();
+      const adminData = responseData[0];
+      console.log(adminData);
+      loginRedirect(adminData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await fetch(`${serverUrl}/admin`);
+      const responseData = await response.json();
+      const adminData = responseData[0];
+
+      const adminResponse = await fetch(`${serverUrl}/admin/logout`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: adminData.username }),
+      });
+      const adminResponseData = await adminResponse.json();
+      console.log(adminResponseData);
+      loginRedirect(adminData);
+      location.reload();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import serverUrl from '@/config/serverUrl';
+import FormFieldInput from '@/components/FormField/FormFieldInput';
+import Submit from '@/components/FormField/Submit';
+
+export default function Login() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    getAdmin();
+  }, []);
+
+  const loginRedirect = (info) => {
+    if (info.loggedIn) {
+      push('/dashboard');
+    }
+  };
+
+  const getAdmin = async () => {
+    try {
+      const response = await fetch(`${serverUrl}/admin`);
+      const responseData = await response.json();
+      const adminData = responseData[0];
+      loginRedirect(adminData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    const formInfo = event.target;
+
+    let loginData = {};
+
+    loginData['username'] = formInfo.username.value;
+    loginData['password'] = formInfo.password.value;
+
+    console.log('Login data:', loginData);
+
+    try {
+      const response = await fetch(`${serverUrl}/admin/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+      const responseData = await response.json();
+      loginRedirect(responseData.info);
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+
+    event.preventDefault();
+    formInfo.reset();
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} method="POST">
+        <FormFieldInput type="text" id="username" label="Admin username" />
+        <FormFieldInput type="password" id="password" label="Password" />
+        <Submit title="Login to Dashboard" />
+      </form>
+    </div>
+  );
+}
