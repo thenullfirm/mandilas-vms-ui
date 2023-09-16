f2f141856b68dea95940302525d8e26fe016e5b3
nullthefirst <tey@nullnow.com>
2023-09-16 04:17:28 +0100

    Added logout, getAdmin, getVisitors

---
+++ config/getAdmin.js
@@ -0,0 +1,14 @@
+import serverUrl from './serverUrl';
+
+const getAdmin = async (loginCheck) => {
+  try {
+    const response = await fetch(`${serverUrl}/admin`);
+    const responseData = await response.json();
+    const adminData = responseData[0];
+    loginCheck(adminData);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+export default getAdmin;
