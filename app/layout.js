841df15f9a34356718684f8a2857d633252dba2c
nullthefirst <tey@nullnow.com>
2023-09-16 03:00:00 +0100

    Removed chakra-ui

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
