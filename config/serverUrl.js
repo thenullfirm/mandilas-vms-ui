e0044e557c0543d567a88aebab3f3b88f1290b4f
nullthefirst <tey@nullnow.com>
2023-09-17 16:47:14 +0100

    Updated serverUrl config

--- config/serverUrl.js
+++ config/serverUrl.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 let serverUrl = process.env.NEXT_PUBLIC_API_URL;
-serverUrl = serverUrl.slice(1, -2);
+serverUrl = process.env.NEXT_PUBLIC_ENV === "'local';" ? serverUrl.slice(1, -2) : process.env.NEXT_PUBLIC_API_URL;
 
 export default serverUrl;
