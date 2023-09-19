b40b031c47e54f80143738560bc90ffe67c3f8ef
nullthefirst <tey@nullnow.com>
2023-09-19 21:22:20 +0100

    Completed employee creation form

---
+++ pages/dashboard/employee.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import getAdmin from '@/config/getAdmin';
+import logout from '@/config/logout';
+import serverUrl from '@/config/serverUrl';
+import { useRouter } from 'next/navigation';
+import FormFieldInput from '@/components/FormField/FormFieldInput';
+import Submit from '@/components/FormField/Submit';
+
+export default function Dashboard() {
+  const [formData, setFormData] = useState({});
+  const [feedback, setFeedback] = useState({});
+
+  const { push } = useRouter();
+
+  const loginRedirect = (info) => {
+    if (!info.loggedIn) {
+      push('/login');
+    }
+  };
+
+  useEffect(() => {
+    getAdmin(loginRedirect);
+  }, []);
+
+  const handleSubmit = async (event) => {
+    const formInfo = event.target;
+
+    let employeeForm = {};
+
+    employeeForm['employeeName'] = formInfo.employeeName.value;
+    employeeForm['employeeEmail'] = formInfo.employeeEmail.value;
+
+    try {
+      const response = await fetch(`${serverUrl}/employees`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(employeeForm),
+      });
+      const responseData = await response.json();
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+
+    event.preventDefault();
+    formInfo.reset();
+  };
+
+  return (
+    <div>
+      <h1>Add Employee</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <FormFieldInput type="text" id="employeeName" label="Employee name" />
+          <FormFieldInput type="text" id="employeeEmail" label="Employee email" />
+          <Submit title="Add Employee" />
+        </form>
+      </div>
+      <button onClick={() => logout(loginRedirect)}>Logout</button>
+    </div>
+  );
+}
