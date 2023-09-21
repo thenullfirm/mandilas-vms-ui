74113a77c9eb619ba9e1d51d36cc9d83c6449d6c
nullthefirst <tey@nullnow.com>
2023-09-21 11:39:05 +0100

    Updated serverUrl config

--- config/serverUrl.js
+++ config/serverUrl.js
@@ -2,7 +2,6 @@ import localEnvParser from './localEnvParser';
 
 require('dotenv').config();
 
-let serverUrl = process.env.NEXT_PUBLIC_API_URL;
-serverUrl = process.env.NEXT_PUBLIC_ENV === "'local';" ? localEnvParser(serverUrl) : process.env.NEXT_PUBLIC_API_URL;
+const serverUrl = localEnvParser(process.env.NEXT_PUBLIC_API_URL);
 
 export default serverUrl;
