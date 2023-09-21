e9afae1442c9dd6ab1052e3e0642a420f1ccb74b
nullthefirst <tey@nullnow.com>
2023-09-21 11:20:48 +0100

    Implemented localEnvParser in serverUrl file

--- config/serverUrl.js
+++ config/serverUrl.js
@@ -1,6 +1,8 @@
+import localEnvParser from './localEnvParser';
+
 require('dotenv').config();
 
 let serverUrl = process.env.NEXT_PUBLIC_API_URL;
-serverUrl = process.env.NEXT_PUBLIC_ENV === "'local';" ? serverUrl.slice(1, -2) : process.env.NEXT_PUBLIC_API_URL;
+serverUrl = process.env.NEXT_PUBLIC_ENV === "'local';" ? localEnvParser(serverUrl) : process.env.NEXT_PUBLIC_API_URL;
 
 export default serverUrl;
